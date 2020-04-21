f9534de7969274362c9f9726bbedc946938e89c1
Chiu Wah Li <chiuwah@gmail.com>
2020-04-21 21:33:56 +0200

    29 Generating access tokens (not finished)

--- src/domain/access_token/access_token.go
+++ src/domain/access_token/access_token.go
@@ -1,13 +1,17 @@
 package access_token
 
 import (
+	"fmt"
 	"github.com/chiuwah/fd_oauth-api/src/utils/errors"
+	"github.com/chiuwah/fd_users-api/utils/crypto_utils"
 	"strings"
 	"time"
 )
 
 const (
-	expirationTime = 24
+	expirationTime             = 24
+	grantTypePassword          = "password"
+	grantTypeClientCredentials = "client_credentials"
 )
 
 type AccessToken struct {
@@ -15,6 +19,33 @@ type AccessToken struct {
 	UserId      int64  `json:"user_id"`
 	ClientId    int64  `json:"client_id"`
 	Expires     int64  `json:"expires"`
+}
+
+type AccessTokenRequest struct {
+	GrantType string `json:"grant_type"`
+	Scope     string `json:"scope"`
+
+	// Used for password grant type
+	Username string `json:"username"`
+	Password string `json:"password"`
+
+	// Used for client_credentials grant type
+	ClientId     string `json:"client_id"`
+	ClientSecret string `json:"client_secret"`
+}
+
+func (at *AccessTokenRequest) Validate() *errors.RestErr {
+	switch at.GrantType {
+	case grantTypePassword:
+		break
+	case grantTypeClientCredentials:
+		break
+	default:
+		return errors.NewBadRequestError("invalid grant_type parameter")
+	}
+
+	//TODO: Validate parameters for each grant_type
+	return nil
 }
 
 func (at *AccessToken) Validate() *errors.RestErr {
@@ -34,8 +65,9 @@ func (at *AccessToken) Validate() *errors.RestErr {
 	return nil
 }
 
-func GetNewAccessToken() *AccessToken {
-	return &AccessToken{
+func GetNewAccessToken(userId int64) AccessToken {
+	return AccessToken{
+		UserId:  userId,
 		Expires: time.Now().UTC().Add(expirationTime * time.Hour).Unix(),
 	}
 }
@@ -44,4 +76,8 @@ func (at AccessToken) IsExpired() bool {
 	now := time.Now().UTC()
 	expirationTime := time.Unix(at.Expires, 0)
 	return now.After(expirationTime)
+}
+
+func (at *AccessToken) Generate() {
+	at.AccessToken = crypto_utils.GetMd5(fmt.Sprintf("at-%d-%d-ran", at.UserId, at.Expires))
 }
--- src/services/access_token/access_token_service.go
+++ src/services/access_token/access_token_service.go
@@ -2,29 +2,33 @@ package access_token
 
 import (
 	"github.com/chiuwah/fd_oauth-api/src/domain/access_token"
+	"github.com/chiuwah/fd_oauth-api/src/repository/db"
+	"github.com/chiuwah/fd_oauth-api/src/repository/rest"
 	"github.com/chiuwah/fd_oauth-api/src/utils/errors"
 	"strings"
 )
 
 type Repository interface {
 	GetById(string) (*access_token.AccessToken, *errors.RestErr)
-	Create(access_token.AccessToken) *errors.RestErr
+	Create(access_token.AccessTokenRequest) (*access_token.AccessToken, *errors.RestErr)
 	UpdateExpirationTime(access_token.AccessToken) *errors.RestErr
 }
 
 type Service interface {
-	GetById(string) (*access_token.AccessToken, *errors.RestErr)
+	GetById(string) (*access_token.AccessToken, errors.RestErr)
 	Create(access_token.AccessToken) *errors.RestErr
 	UpdateExpirationTime(access_token.AccessToken) *errors.RestErr
 }
 
 type service struct {
-	repository Repository
+	restUsersRepo rest.RestUsersRepository
+	dbRepo        db.DbRepository
 }
 
-func NewService(repo Repository) Service {
+func NewService(usersRepo rest.RestUsersRepository, dbRepo db.DbRepository) *service {
 	return &service{
-		repository: repo,
+		restUsersRepo: usersRepo,
+		dbRepo:        dbRepo,
 	}
 }
 
@@ -33,23 +37,40 @@ func (s *service) GetById(accessTokenId string) (*access_token.AccessToken, *err
 	if len(accessTokenId) == 0 {
 		return nil, errors.NewBadRequestError("invalid access token id")
 	}
-	accessToken, err := s.repository.GetById(accessTokenId)
+	accessToken, err := s.dbRepo.GetById(accessTokenId)
 	if err != nil {
 		return nil, err
 	}
 	return accessToken, nil
 }
 
-func (s *service) Create(at access_token.AccessToken) *errors.RestErr {
-	if err := at.Validate(); err != nil {
-		return err
+func (s *service) Create(request access_token.AccessTokenRequest) (*access_token.AccessToken, *errors.RestErr) {
+	if err := request.Validate(); err != nil {
+		return nil, err
 	}
-	return s.repository.Create(at)
+
+	// TODO: Support both grant types: client_credentials and password
+
+	// Authenticate the user against the Users API:
+	user, err := s.restUsersRepo.LoginUser(request.Username, request.Password)
+	if err != nil {
+		return nil, err
+	}
+
+	// Generate a new access token:
+	at := access_token.GetNewAccessToken(user.Id)
+	at.Generate()
+
+	// Save the new access token in Cassandra:
+	if err := s.dbRepo.Create(at); err != nil {
+		return nil, err
+	}
+	return &at, nil
 }
 
 func (s *service) UpdateExpirationTime(at access_token.AccessToken) *errors.RestErr {
 	if err := at.Validate(); err != nil {
 		return err
 	}
-	return s.repository.UpdateExpirationTime(at)
+	return s.dbRepo.UpdateExpirationTime(at)
 }
